68539b70463e72e37c003579224325f2eae3c003
Stelios Vitalis <steliosvitalis@gmail.com>
2020-04-29 18:47:02 +0200

    Add 3D tiles in the scene

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ require('./css/main.css');
 require('cesium/Widgets/widgets.css');
 
 var viewer = new Cesium.Viewer('cesiumContainer');
+
+var city = viewer.scene.primitives.add(new Cesium.Cesium3DTileset({ url: '/data/tileset.json'}));
