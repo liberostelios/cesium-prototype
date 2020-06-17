8ef3e43c59523693ef90fe8f714de393d97b5d13
Stelios Vitalis <steliosvitalis@gmail.com>
2020-06-17 15:48:17 +0200

    Remove data from repo and make viewer zoom to tileset

--- src/index.js
+++ src/index.js
@@ -6,3 +6,5 @@ require('cesium/Widgets/widgets.css');
 var viewer = new Cesium.Viewer('cesiumContainer');
 
 var city = viewer.scene.primitives.add(new Cesium.Cesium3DTileset({ url: '/data/tileset.json'}));
+
+viewer.zoomTo(city, new Cesium.HeadingPitchRange(0, -0.5, 0));
